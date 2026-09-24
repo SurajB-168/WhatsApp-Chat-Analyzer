2c1bd5483ba0de8ec524d998813a1011e5f00022
Surajkumar Bevnale <surajk.b168@gmail.com>
2026-09-24 22:36:33 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,51 +1,53 @@
 import re
 import pandas as pd
 
-def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+def preprocessing(data):
+    split_pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s?[AP]M\s-\s'
+    date_pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s?[AP]M'
 
-    messages = re.split(pattern, data)[1:]
-    dates = re.findall(pattern, data)
 
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    messages = re.split(split_pattern, data.replace('\u202f', ''))[1:]
+    dates = re.findall(date_pattern, data.replace('\u202f', ''))
 
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    df = pd.DataFrame({'user_messages': messages, 'date': dates})
+    df['user_messages'] = df['user_messages'].str.replace('\n', '').str.strip()
+    df['date'] = df['date'].str.replace(',', '').str.strip()
 
-    users = []
+    user = []
     messages = []
-    for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
-            users.append(entry[1])
-            messages.append(" ".join(entry[2:]))
+    for message in df['user_messages']:
+        entry = re.split(r'([\w\W]+?):\s', message, maxsplit=1)
+        if entry[1:]:
+            user.append(entry[1])
+            messages.append(entry[2])
         else:
-            users.append('group_notification')
+            user.append('Group Notification')
             messages.append(entry[0])
 
-    df['user'] = users
+    df['user'] = user
     df['message'] = messages
-    df.drop(columns=['user_message'], inplace=True)
+    df.drop(columns=['user_messages'])
 
-    df['only_date'] = df['date'].dt.date
+    df['date'] = pd.to_datetime(df['date'], format='mixed')
     df['year'] = df['date'].dt.year
     df['month_num'] = df['date'].dt.month
     df['month'] = df['date'].dt.month_name()
     df['day'] = df['date'].dt.day
     df['day_name'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
-    df['minute'] = df['date'].dt.minute
+
 
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+    for hour in df['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append(str(hour) + '-' + str('00'))
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append(str('00')+ '-' + str(hour + 1))
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
+            period.append(str(hour) + '-' + str(hour + 1))
 
     df['period'] = period
+
+    df = df[['date', 'user', 'message', 'year', 'month_num', 'month', 'day', 'day_name', 'hour', 'period']]
 
     return df
